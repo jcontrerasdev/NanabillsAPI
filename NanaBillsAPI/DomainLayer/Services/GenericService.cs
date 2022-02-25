2cf4072fcf0878b8eafcf8a96eb6aeac42fefe1a
Juan Carlos Contreras <cmjcarlos7@gmail.com>
2022-02-25 21:54:22 +0100

    Generic service created.

---
+++ NanaBillsAPI/DomainLayer/Services/GenericService.cs
@@ -0,0 +1,47 @@
+﻿using DataAccessLayer.Repositories;
+using System.Linq.Expressions;
+
+namespace DomainLayer.Services
+{
+    internal class GenericService<TEntity, TId> : IGenericService<TEntity, TId> where TEntity : class
+    {
+        private IGenericRepository<TEntity, TId> _genericRepository;
+
+        public GenericService(IGenericRepository<TEntity, TId> genericRepository)
+        {
+            _genericRepository = genericRepository;
+        }
+        public async Task<IEnumerable<TEntity>> GetAll()
+        {
+            return await _genericRepository.GetAll();
+        }
+
+        public async Task<TEntity> GetById(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _genericRepository.GetById(predicate);
+        }
+
+        public async Task<TEntity> Insert(TEntity entity)
+        {
+            return await _genericRepository.Insert(entity);
+        }
+
+        public async Task<TEntity> Update(TEntity entity)
+        {
+            return await _genericRepository.Update(entity);
+        }
+        public async Task Delete(Expression<Func<TEntity, bool>> predicate)
+        {
+            await _genericRepository.Delete(predicate);
+        }
+        public async Task<IEnumerable<TEntity>> Search(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _genericRepository.Search(predicate);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetByUserId(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _genericRepository.GetByUserID(predicate);
+        }
+    }
+}
---
+++ NanaBillsAPI/DomainLayer/Services/IGenericService.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+
+namespace DomainLayer.Services
+{
+    public interface IGenericService<TEntity, TId> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity> GetById(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> Insert(TEntity entity);
+        Task<TEntity> Update(TEntity entity);
+        Task Delete(Expression<Func<TEntity, bool>> predicate);
+        Task<IEnumerable<TEntity>> Search(Expression<Func<TEntity, bool>> predicate);
+        Task<IEnumerable<TEntity>> GetByUserId(Expression<Func<TEntity, bool>> predicate);
+    }
+}
